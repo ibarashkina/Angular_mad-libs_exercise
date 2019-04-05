ace09d402c9e8e41e63fe6cda9bf7b83f2b17de7
Ilona <ilonabarashkina@gmail.com>
2019-04-04 23:31:57 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mad-libs';
+  title:string = 'Mad-libs';
+  
+  noun:string=null;
+  adverb:string=null;
+  hideTitle:boolean=false;
+  // showHide:boolean= false;
+
+  // Cntl1=(hide)=>{
+  //   this.hide.boolean = false;
+  // }
+
+  // var app = angular.module('myApp', []);
+  // app.controller('personCtrl', function($scope) {
+  //     $scope.name = "advert",
+  //     $scope.name = "noun"
+  //     $scope.myVar = false;
+  //     $scope.toggle = function() {
+  //         $scope.myVar = !$scope.myVar;
+  //     };
+  // });
+
+  oneCtrl=($scope)=> {
+    $scope.hide = "";
+}
+
+toggleHeader=()=>{
+  this.hideTitle=!this.hideTitle;
+};
+
 }
